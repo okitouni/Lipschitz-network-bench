e07c058479f1cba3da6d2bcc4ed4645a0b32b9a9
okitouni <okitouni@users.noreply.github.com>
2022-12-14 01:52:55 -0500

    wider 512 MLP

--- cifar10_random_labels.py
+++ cifar10_random_labels.py
@@ -9,36 +9,32 @@ from torchmetrics.functional import accuracy
 from gradient_descent_the_ultimate_optimizer import gdtuo
 import wandb
 
-wandb.init(project="CIFAR10-Rand", entity="iaifi", name="CIFAR10-Rand-MLP-Adam-CE-100-100-5-1e-3")
+wandb.init(project="CIFAR10-Rand", entity="iaifi", name="CIFAR10-Rand-MLP-Adam-CE-100-512-5-1e-3")
 
+LR = 1e-3
+EPOCHS = 100
+BATCH_SIZE = 128
 
 # Set up the dataset
 dataset = CIFAR10(root='data', train=True, download=True, transform=transforms.ToTensor())
 # randomize labels
 dataset.targets = torch.randint(0, 10, (len(dataset),))
-dataloader = DataLoader(dataset, batch_size=128, shuffle=True)
-# %%
-print(dataset[0][0].shape)
+dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
 # %%
 # Set up the model
-max_norm = 2.0
-
 model = torch.nn.Sequential(
     torch.nn.Flatten(),
-    torch.nn.Linear(3072, 100),
+    torch.nn.Linear(3072, 512),
     torch.nn.ReLU(),
-    torch.nn.Linear(100, 100),
+    torch.nn.Linear(512, 512),
     torch.nn.ReLU(),
-    torch.nn.Linear(100, 100),
+    torch.nn.Linear(512, 512),
     torch.nn.ReLU(),
-    torch.nn.Linear(100, 100),
+    torch.nn.Linear(512, 512),
     torch.nn.ReLU(),
-    torch.nn.Linear(100, 10),
+    torch.nn.Linear(512, 10),
 )
 
-LR = 1e-3
-EPOCHS = 100
-BATCH_SIZE = 128
 
 wandb.config = {
     "learning_rate": LR,
@@ -48,7 +44,7 @@ wandb.config = {
     "optimizer": "Adam",
     "loss": "CrossEntropy",
     "dataset": "CIFAR10",
-    "width": 100,
+    "width": 512,
     "depth": 5,
     "activation": "ReLU",
 }
@@ -61,6 +57,8 @@ optimizer = torch.optim.Adam(model.parameters(), lr=LR)
 # Train the model
 pbar = tqdm.tqdm(range(EPOCHS))
 for epoch in pbar:
+    agg_loss = 0
+    agg_acc = 0
     for batch in dataloader:
         x, y = batch
         # mw.begin() # call this before each step, enables gradient tracking on desired params
@@ -78,7 +76,9 @@ for epoch in pbar:
         
         acc = accuracy(pred, y, num_classes=10)
         pbar.set_description(f'Epoch {epoch}, loss {loss.item():.3f}, acc {acc:.3f}')
-        wandb.log({"loss": loss, "acc": acc})
-        wandb.watch(model)
+        agg_loss += loss.item()/len(dataloader)
+        agg_acc += acc/len(dataloader)
+    wandb.log({"loss": agg_loss, "acc": agg_acc})
+    wandb.watch(model)
 
 
